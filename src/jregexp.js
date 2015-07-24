7706b30da5637b25e601b63c08f5a14a3fc63209
CecilLee <cciscecil@gmail.com>
2015-07-24 14:52:40 +0800

    完善正则表达式文法定义

--- src/jregexp.js
+++ src/jregexp.js
@@ -10,6 +10,8 @@ var jregexp = (function(){
 
     var jcon = require('jcon');
 
+    var epsilon = jcon.string('');
+
     var metaChr = jcon.or(
         jcon.string('\\'),
         jcon.string('^'),       //起始符
@@ -19,18 +21,90 @@ var jregexp = (function(){
         jcon.string('?'),       //零或一次
         jcon.string('.'),       //any char
         jcon.string('{'),       //指定次数闭包
+        jcon.string('}'),       //指定次数闭包
         jcon.string('['),       //range start chr
-        jcon.string('(')       //group start chr
+        jcon.string(']'),       //range end chr
+        jcon.string('('),       //group start chr
+        jcon.string(')')        //group end chr
     );
 
-    var chr = jcon.or(
+    var nonMetaChr = jcon.or(
         jcon.not(metaChr).setAst('chr'),
         jcon.seq(jcon.string('\\'), metaChr.setAst('chr'))
     );
 
+    var range = jcon.seq(
+        nonMetaChr.setAst('range-start'),
+        jcon.string('-'),
+        nonMetaChr.setAst('range-end')
+    ).setAst('range');
 
+    var item = jcon.or(range, nonMetaChr);
 
-    return chr;
+    var items = item.least(1);
+
+    var positiveSet = jcon.seq(
+        jcon.string('['),
+        items,
+        jcon.string(']')
+    ).setAst('positive-set');
+
+    var negativeSet = jcon.seq(
+        jcon.string('[^'),
+        items,
+        jcon.string(']')
+    ).setAst('negative-set');
+
+    var set = jcon.or(negativeSet, positiveSet);
+
+    var eos = jcon.string('$');
+    var any = jcon.string('.');
+    var group = jcon.seq(
+        jcon.string('('),
+        jcon.lazy(function(){
+            return union;
+        }),
+        jcon.string(')')
+    );
+    var reElementary = jcon.or(
+        group,
+        any,
+        eos,
+        nonMetaChr,
+        set
+    );
+    var plus = jcon.seq(
+        reElementary,
+        jcon.string('+')
+    );
+    var star = jcon.seq(
+        reElementary,
+        jcon.string('*')
+    );
+    var basicRe = jcon.or(
+        star,
+        plus,
+        reElementary
+    );
+    var concatenation = basicRe.least(1).setAst('concatenation');
+
+    var union = jcon.lazy(function(){
+        return jcon.seq(
+            concatenation,
+            unionRest
+        );
+    });
+    var unionRest = jcon.or(
+        epsilon,
+        jcon.seq(
+            jcon.string('|'),
+            union
+        )
+    );
+
+    var re = union;
+
+    return re;
 
 }());
 
--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 var jregexp = require('./src/jregexp.js');
 
-console.log( jregexp.parse('\\').ast() );
-console.log( jregexp.parse('\\\\').ast() );
+console.log( JSON.stringify(jregexp.parse('a[abA-Z]').ast(), null, ' ') );
 
+/*
+var jcon = require('jcon');
+
+console.log( jcon.seq(jcon.string('['), jcon.not(jcon.string(']')), jcon.string(']')).parse('[a]') );
+*/
 
 //jregexp.parse('abc(?:ab|[^b|c]+|\d)*');
